747487e7d42c0093f141a30fea859098b8d72d7d
Vishwas Puri <80814270+vishwaspuriofficial@users.noreply.github.com>
2021-10-21 17:31:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ st.subheader("Made by Vishwas Puri using Streamlit (python)")
 num1 = st.number_input('Enter Number 1:')
 num2 = st.number_input('Enter Number 2:')
 
-st.write(f"The sum is: {num1 + num2}")
+st.write(f"The product is: {num1 * num2}")
